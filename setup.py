1010060063b03816a9c496f59d6e89989c2c3280
Amir Alavi <s.amir.alavi@gmail.com>
2018-06-05 12:29:08 -0400

    Added license and github url

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ setup(name='sparsely_connected_keras',
       version='0.1.0',
       description='Sparsely-connected layers for Keras',
       author='Amir Alavi',
+      url='https://github.com/AmirAlavi/sparsely-connected-keras',
+      license='GPLv3',
       packages=find_packages(),
       install_requires=[
           'keras>=2',
           'numpy',
       ],
       python_requires='>=3'
-)
+      )
