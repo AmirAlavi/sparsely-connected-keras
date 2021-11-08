f636716c2a5e6df4498734876b29eca02f8ee8fc
ichbinram <ramprasadraghunath@gmail.com>
2021-11-08 18:59:12 +0100

    updated for tf-2 version

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='sparsely_connected_keras',
-      version='1.0.0',
+      version='2.0.0',
       description='Sparsely-connected layers for Keras',
-      author='Amir Alavi',
-      url='https://github.com/AmirAlavi/sparsely-connected-keras',
+      author='Ramprasad',
+      url='https://github.com/ichbinram/sparsely-connected-keras',
       license='GPLv3',
       packages=find_packages(),
       install_requires=[
-          'keras>=2',
+          'tensorflow>=2',
           'numpy',
       ],
       python_requires='>=3'
